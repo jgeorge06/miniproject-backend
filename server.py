81e1757642f975821a19e78fb856acb52f5297a4
jgeorge06 <jgeorgenedumala@gmail.com>
2025-03-15 20:06:50 +0530

    fixed server.py
    imported the right module
    allowed it to accept request from all sources

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask,request,jsonify
 from jobfind import *
-import sys
+import os
+from flask_cors import CORS
 
 app=Flask(__name__)
-
+CORS(app)
 
 @app.route('/job_finder',methods=['POST'])
 def job_finder():
