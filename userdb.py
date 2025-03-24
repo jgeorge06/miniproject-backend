99df58b76b5ab97fa0a23a95c032888d2d623dd6
jgeorge06 <jgeorgenedumala@gmail.com>
2025-03-24 20:38:52 +0530

    fixedquerry

--- userdb.py
+++ userdb.py
@@ -21,7 +21,7 @@ def retrieve(user):
     try:
         mydb=pymysql.connect(host=os.getenv("MYSQLHOST"),user=os.getenv("MYSQLUSER"), passwd=os.getenv("MYSQLPASSWORD"), database=os.getenv("MYSQLDATABASE"),cursorclass=pymysql.cursors.Cursor)
         with mydb.cursor() as cursor:
-            cursor.execute("select * from users where uid='%s'",user)
+            cursor.execute("select * from users where uid='%s';",user)
             data = cursor.fetchall()
         mydb.close()
         print(data)
